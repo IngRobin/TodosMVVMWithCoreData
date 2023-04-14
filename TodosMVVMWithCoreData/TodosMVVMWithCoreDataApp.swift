11a48113bcf63555f5d3253757e802614e519f5b
Robinson Gonzalez <rgonzalezr0322@gmail.com>
2023-04-14 17:01:01 -0500

    Initial Commit

---
+++ TodosMVVMWithCoreData/TodosMVVMWithCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosMVVMWithCoreDataApp.swift
+//  TodosMVVMWithCoreData
+//
+//  Created by Robinson Gonzalez on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TodosMVVMWithCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
